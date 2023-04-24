2d303fef8006bb3e8a1dd4ba25c9acca48da8c03
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-04-24 19:09:06 +0530

    async and await

--- src/App.js
+++ src/App.js
@@ -1,65 +1,59 @@
-import './App.scss';
-import Search from './components/SearchBar/Search';
-import Movies from './components/Movies/Movies';
-import { Fragment, useState } from 'react';
+import "./App.scss";
+import Search from "./components/SearchBar/Search";
+import Movies from "./components/Movies/Movies";
+import { Fragment, useState } from "react";
 
 const Dummy_Data = [
-    {
-        title: "batman city",
-        id: "1",
-        date: "2018",
-        details: "hello",
-    },
-    {
-        title: "spiderman",
-        id: "2",
-        date: "2020",
-        details: "hello",
-    },
-    {
-        title: "assassin creed",
-        id: "3",
-        date: "1019",
-        details: "hello",
-    },
+  {
+    title: "batman city",
+    id: "1",
+    date: "2018",
+    details: "hello",
+  },
+  {
+    title: "spiderman",
+    id: "2",
+    date: "2020",
+    details: "hello",
+  },
+  {
+    title: "assassin creed",
+    id: "3",
+    date: "1019",
+    details: "hello",
+  },
 ];
 
 // const API_URL = 'https://www.omdbapi.com?apikey=3485b64c';
-const API_URL = 'https://swapi.dev/api/films/';
+const API_URL = "https://swapi.dev/api/films/";
 
 const App = () => {
-    const [allMovies, setAllMovies] = useState(Dummy_Data);
+  const [allMovies, setAllMovies] = useState(Dummy_Data);
 
-    function fetchMovies() {
-        fetch(API_URL).then(response => {
-            return response.json();
-        }).then(data => {
-            const transformedResults = data.results.map( data => {
-                return {
-                    title: data.title,
-                    id: data.episode_id,
-                    details: data.opening_crawl,
-                    date: data.release_date,
-                }
-            })
-            setAllMovies(transformedResults);
-        });
+  async function fetchMovies() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    const transformedResults = data.results.map((data) => {
+      return {
+        title: data.title,
+        id: data.episode_id,
+        details: data.opening_crawl,
+        date: data.release_date,
+      };
+    });
+    setAllMovies(transformedResults);
+  }
 
-        
-    }
+  const data = (movie) => {
+    fetchMovies();
+  };
 
-    const data =(movie) => {
-        fetchMovies();
-    }
-    
-
-    return (
-        <Fragment>
-            <Search item={data}/>
-            <Movies alldata = {allMovies}/>
-        </Fragment>
-       
-    )
-}
+  return (
+    <Fragment>
+      <Search item={data} />
+      <Movies alldata={allMovies} />
+    </Fragment>
+  );
+};
 
 export default App;
