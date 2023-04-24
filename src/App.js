e51661a38afaf0f12f7099d588f38f77c2b86757
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-04-25 03:32:45 +0530

    loading and data states

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import "./App.scss";
 import Search from "./components/SearchBar/Search";
 import Movies from "./components/Movies/Movies";
-import { Fragment, useState, useTransition } from "react";
+import { Fragment, useState } from "react";
 
 
-const API_URL = 'https://www.omdbapi.com?apikey=3485b64c&s=spiderman';
+const API_URL = 'https://www.omdbapi.com?apikey=3485b64c';
 // const API_URL = "https://swapi.dev/api/films/";
 
 const App = () => {
   const [allMovies, setAllMovies] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  async function fetchMovies() {
+
+
+  async function fetchMovies(movie) {
     setLoading(true);
-    const response = await fetch(API_URL);
+    const response = await fetch(`${API_URL}&s=${movie}`);
     const data = await response.json();
-    console.log(data.Search);
     const transformedResults = data.Search.map((data, i) => {
       return {
         title: data.Title,
@@ -30,7 +31,7 @@ const App = () => {
   }
 
   const data = (movie) => {
-    fetchMovies();
+    fetchMovies(movie);
   };
 
   return (
@@ -38,7 +39,7 @@ const App = () => {
       <Search item={data} />
       <div className='movcontainer card'>
       {!loading && allMovies.length > 0 && <Movies alldata={allMovies} />}
-      {!loading && allMovies.length === 0 && <p>No data found.</p> }
+      {!loading && allMovies.length === 0  && <p>No data found.</p> }
       {loading && <p>Loading....</p>}
       </div>
     </Fragment>
--- src/components/SearchBar/Search.js
+++ src/components/SearchBar/Search.js
@@ -1,18 +1,29 @@
-import './Search.scss';
-
-
+import "./Search.scss";
+import { useState } from "react";
 
 const Search = (props) => {
-    const onClickHandler = () => {
-        props.item([]);
-    }
-    return (
-        <div className='container'>
-             <input className='movie-search card' type='text' placeholder='Enter the name of the movie'></input>
-             <button onClick={onClickHandler} className='btn card'>Search</button>
-        </div>
-       
-    )
-}
+  const [movie, setMovie] = useState("");
+
+  const onClickHandler = () => {
+    props.item(movie);
+  };
+
+  return (
+    <div className="container">
+      <input
+        value={movie}
+        onChange={(e) => {
+          setMovie(e.target.value);
+        }}
+        className="movie-search card"
+        type="text"
+        placeholder="Enter the name of the movie"
+      ></input>
+      <button onClick={onClickHandler} className="btn card">
+        Search
+      </button>
+    </div>
+  );
+};
 
 export default Search;
